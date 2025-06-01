1d9c591ae24b15f866f9e75d1660a56ff9c4e50c
Talha Khan <talha.khan198908@gmail.com>
2025-06-01 16:21:57 +0100

    Add files via upload

---
+++ Basic_commands.py
@@ -0,0 +1,80 @@
+name = "talha"
+age = 31
+print("my name is", name, "and I am", age, "years old")
+print(type(age))
+print(type(name))
+
+#arithmetic operators
+a = 9
+b = 2
+sum = a+b
+print(a+b)
+print(a - b)
+print(a / b) # Divide
+print(a*b) # Multiply
+
+print(a ** b)
+print(a%b)
+
+#Relational operators
+a = 25
+b = 20
+print(a == b) #false
+print(a != b) #True
+print(a <= b) #false
+print(a >= b) #True
+
+#Assignment Operators
+num = 10
+num = num + 10
+print(num)
+
+num = 5
+num += 5
+print(num)
+
+a = 20
+a -= 20
+print(a)
+
+b = 15
+b *= 15
+print(b)
+
+num = 26
+num = num + 26
+print("num", num)
+
+#Type conversion
+a = int("4")
+b = 6.5
+print(a + b)
+
+a = float("4")
+b = 6.5
+print(a + b)
+
+a = int("4")
+print(type(a))
+
+a = 4.15
+a = str(a)
+print(type(a))
+
+name = ("Don")
+age = 30
+amount = 100000
+friends = ("Ali, Omar, Usman and Abubakar")
+print("my name is", name, "and I am", age, "years of age.", "I think I won a lottery of", amount, "I can't now wait to share this news with", friends,)
+
+#How to take input from the User from the terminal.
+name = input("enter your name:")
+print("welcome", name)
+
+age = input("enter your age:")
+print("Did you say, you are", age, "mate?")
+
+course = input("what course did you sign up on?:")
+print("Well done, Mate!")
+
+             
